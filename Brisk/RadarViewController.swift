f91a7d68c41a7655d69d4938cccf118f6b58f0c2
Keith Smiley <keithbsmiley@gmail.com>
2016-06-17 19:37:04 -0700

    I'll explain this when I'm sober .. or revert it

--- Brisk/RadarViewController.swift
+++ Brisk/RadarViewController.swift
@@ -38,40 +38,86 @@ final class RadarViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupTextViewDelegates()
     }
 
     @IBAction private func submitRadar(sender: AnyObject) {
+        guard let radar = self.currentRadar() else {
+            return
+        }
+
+        _ = radar
     }
 
     // MARK: - Private Methods
 
     private func currentRadar() -> Radar? {
+        for field in self.validatables {
+            if !field.isValid {
+                return nil
+            }
+        }
+
         guard let product = self.products.find({ $0.name == self.productPopUp.selectedTitle }),
-            let classification = self.classifications.find({ $0.name == self.classificationPopUp.selectedTitle }),
-            let reproducibility = self.reproducibility.find({ $0.name == self.reproducibilityPopUp.selectedTitle }),
-            let area = self.areas.find({ $0.name == self.areaPopUp.selectedTitle }),
-            let description = self.descriptionTextView.string,
-            let steps = self.stepsTextView.string,
-            let expected = self.expectedTextView.string,
-            let actual = self.actualTextView.string,
-            let notes = self.notesTextView.string else
+            let classification = self.classifications
+                .find({ $0.name == self.classificationPopUp.selectedTitle }),
+            let reproducibility = self.reproducibility
+                .find({ $0.name == self.reproducibilityPopUp.selectedTitle }),
+            let area = self.areas.find({ $0.name == self.areaPopUp.selectedTitle }) else
         {
             return nil
         }
 
-        let title = self.titleTextField.stringValue
+        let actual = self.actualTextView.stringValue
         let configuration = self.configurationTextField.stringValue
+        let description = self.descriptionTextView.stringValue
+        let expected = self.expectedTextView.stringValue
+        let notes = self.notesTextView.stringValue
+        let steps = self.stepsTextView.stringValue
+        let title = self.titleTextField.stringValue
         let version = self.versionTextField.stringValue
 
         return Radar(product: product, classification: classification, reproducibility: reproducibility,
                      area: area, title: title, description: description, steps: steps, expected: expected,
                      actual: actual, configuration: configuration, version: version, notes: notes)
     }
+
+    private func enableSubmitIfValid() {
+        for field in self.validatables {
+            if !field.isValid {
+                self.submitButton.enabled = false
+                return
+            }
+        }
+
+        self.submitButton.enabled = true
+    }
 }
 
 extension RadarViewController: NSTextViewDelegate {
     override func controlTextDidChange(obj: NSNotification) {
-        super.controlTextDidChange(obj)
+//        super.controlTextDidChange(obj)
+        self.enableSubmitIfValid()
+    }
+
+    func textDidChange(notification: NSNotification) {
+        self.enableSubmitIfValid()
+    }
+
+    var textViews: [NSTextView] {
+        return [
+            self.actualTextView,
+            self.descriptionTextView,
+            self.expectedTextView,
+            self.notesTextView,
+            self.stepsTextView,
+        ]
+    }
+
+    func setupTextViewDelegates() {
+        for textView in self.textViews {
+            textView.delegate = self
+        }
     }
 }
 
@@ -83,7 +129,12 @@ extension NSTextView: Validatable {
     var isValid: Bool {
         return self.string?.isEmpty == false
     }
+
+    var stringValue: String {
+        return self.string ?? ""
+    }
 }
+
 extension NSTextField: Validatable {
     var isValid: Bool {
         return !self.stringValue.isEmpty
@@ -92,12 +143,13 @@ extension NSTextField: Validatable {
 
 extension NSPopUpButton: Validatable {
     var isValid: Bool {
-        return self.selectedTitle?.isEmpty == false
+        return true
+        return !self.selectedTitle.isEmpty
     }
 }
 
 extension NSPopUpButton {
-    var selectedTitle: String? {
-        return self.selectedItem?.title
+    var selectedTitle: String {
+        return self.selectedItem?.title ?? ""
     }
 }
