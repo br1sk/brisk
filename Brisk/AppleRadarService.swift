6534dd08a81235ebbb53e1c5400f877c11283924
Scott Berrevoets <s.berrevoets@me.com>
2016-06-17 20:01:26 -0700

    Either Hot Shit or Total Bollocks

--- Brisk/AppleRadarService.swift
+++ Brisk/AppleRadarService.swift
@@ -4,32 +4,31 @@ private let kAppleRadarServiceURL = "http://10.10.248.180/types"
 
 struct AppleRadarService: RadarService {
 
-    func submit(radar radar: Radar) {}
+    func submit(radar radar: Radar) {
+        
+    }
 
     static func retrieveRadarComponents(completion: RadarComponents -> Void) {
-        let sessionConfiguration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        Alamofire.request(.GET, kAppleRadarServiceURL, encoding: .URLEncodedInURL).responseJSON { response in
+            guard let dictionary = response.result.value as? NSDictionary else {
+                return
+            }
 
-        Alamofire.request(.GET, kAppleRadarServiceURL, encoding: .URLEncodedInURL)
-                 .responseJSON { response in
-                    guard let dictionary = response.result.value as? NSDictionary else {
-                        return
-                    }
+            let productDictionaries = dictionary["products"] as? [NSDictionary]
+            let areaDictionaries = dictionary["areas"] as? [NSDictionary]
+            let classificationDictionaries = dictionary["classifications"] as? [NSDictionary]
+            let reproducibilityDictionaries = dictionary["reproducibilities"] as? [NSDictionary]
 
-                    let productDictionaries = dictionary["products"] as? [NSDictionary]
-                    let areaDictionaries = dictionary["areas"] as? [NSDictionary]
-                    let classificationDictionaries = dictionary["classifications"] as? [NSDictionary]
-                    let reproducibilityDictionaries = dictionary["reproducibilities"] as? [NSDictionary]
+            let products = productDictionaries!.flatMap(Product.withDictionary)
+            let areas = areaDictionaries!.flatMap(Area.withDictionary)
+            let classifications = classificationDictionaries!.flatMap(Classification.withDictionary)
+            let reproducibilities = reproducibilityDictionaries!.flatMap(Reproducibility.withDictionary)
 
-                    let products = productDictionaries!.flatMap(Product.withDictionary)
-                    let areas = areaDictionaries!.flatMap(Area.withDictionary)
-                    let classifications = classificationDictionaries!.flatMap(Classification.withDictionary)
-                    let reproducibilities = reproducibilityDictionaries!.flatMap(Reproducibility.withDictionary)
-
-
-                    let components = RadarComponents(products: products, areas: areas,
-                        classifications: classifications, reproducibilities: reproducibilities)
-
-
-                 }
+            dispatch_async(dispatch_get_main_queue()) {
+                let components = RadarComponents(products: products, areas: areas,
+                    classifications: classifications, reproducibilities: reproducibilities)
+                completion(components)
+            }
+        }
     }
 }
