ab312a5b9ccca99b8312fe4ca3d428df82262ef2
Keith Smiley <keithbsmiley@gmail.com>
2016-06-19 13:40:03 -0700

    Is this dependency injection?

--- Brisk/TabViewController.swift
+++ Brisk/TabViewController.swift
@@ -3,12 +3,16 @@ import AppKit
 final class TabViewController: NSTabViewController {
     override func viewWillAppear() {
         super.viewWillAppear()
-        self.view.window?.title = self.tabView.selectedTabViewItem?.viewController?.title ?? ""
+        self.setTitle(withItem: self.tabView.selectedTabViewItem)
     }
 
     override func tabView(tabView: NSTabView, didSelectTabViewItem tabViewItem: NSTabViewItem?) {
         super.tabView(tabView, didSelectTabViewItem: tabViewItem)
-        let newTitle = tabViewItem?.viewController?.title
+        self.setTitle(withItem: tabViewItem)
+    }
+
+    private func setTitle(withItem item: NSTabViewItem?) {
+        let newTitle = item?.viewController?.title
         self.view.window?.title = newTitle ?? ""
     }
 }
