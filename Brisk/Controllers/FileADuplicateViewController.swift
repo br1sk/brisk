105ddabac92ae05aa1e0df78697564efbb24c0b7
Peter Steinberger <steipete@gmail.com>
2017-06-21 20:16:40 +0200

    File a Duplicate feature 💪
    This adds a new button that fetches radars from OpenRadar.

---
+++ Brisk/Controllers/FileADuplicateViewController.swift
@@ -0,0 +1,29 @@
+import Foundation
+import AppKit
+
+final class FileADuplicateViewController: ViewController {
+    @IBOutlet private var radarTextField: NSTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    public var radarID: String {
+        get {
+            return radarTextField.stringValue
+        }
+    }
+
+    @IBAction private func fetchData(_ sender: Any) {
+        closeSheetWithReturnCode(NSModalResponseContinue)
+    }
+
+    @IBAction private func closeSheet(_ sender: Any) {
+        closeSheetWithReturnCode(NSModalResponseStop)
+    }
+
+    private func closeSheetWithReturnCode(_ returnCode: NSApplication.ModalResponse) {
+        view.window?.sheetParent?.endSheet(view.window!, returnCode: returnCode)
+        StoryboardRouter.reloadTopWindowController()
+    }
+}
