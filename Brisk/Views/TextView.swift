64e4be1064623983297cad9ccaefaf0c72d25301
Keith Smiley <keithbsmiley@gmail.com>
2017-06-22 21:18:28 -0700

    Make TextView tab and backtab switch between fields
    Previously this would insert tabs into the textview, but this probably
    makes more sense to navigate between fields. This flips the behavior of
    Ctrl + tab switching between fields to make that actually insert a tab.

--- Brisk/Views/TextView.swift
+++ Brisk/Views/TextView.swift
@@ -6,4 +6,19 @@ final class TextView: NSTextView {
 
         self.font = NSFont.systemFont(ofSize: NSFont.smallSystemFontSize())
     }
+
+    override func doCommand(by selector: Selector) {
+        switch selector {
+        case #selector(TextView.insertTab(_:)):
+            self.window?.selectNextKeyView(self)
+        case #selector(TextView.insertBacktab(_:)):
+            self.window?.selectPreviousKeyView(self)
+        case #selector(NSWindow.selectNextKeyView(_:)):
+            super.doCommand(by: #selector(TextView.insertTab(_:)))
+        case #selector(NSWindow.selectPreviousKeyView(_:)):
+            super.doCommand(by: #selector(TextView.insertBacktab(_:)))
+        default:
+            super.doCommand(by: selector)
+        }
+    }
 }
